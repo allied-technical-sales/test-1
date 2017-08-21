e7bd5d5f1e3740601458fa96b8b7bc34dc8b5851
Ryan Wong <r25wong@gmail.com>
2017-08-21 17:58:52 -0400

    - switched test2.txt to LF ending (from CRLF) - changed tests to reflect new test2.txt file - added tests for +k functionality - added functionality for +k (-n 10 will print last 10 lines, -n +10 will skip 10 lines and print the rest)

--- tail.rb
+++ tail.rb
@@ -29,8 +29,9 @@ OptionParser.new do |opts|
   end
 
   # -n, --lines=K flag
-  opts.on("-n", "--lines=K", Integer, "Output the last K lines, instead of the default of the last 10; alternatively, use \"-n +K\" to output lines starting with the Kth.") do |k|
-    options[:lines] = k
+  opts.on("-n", "--lines=K", "Output the last K lines, instead of the default of the last 10; alternatively, use \"-n +K\" to output lines starting with the Kth.") do |k|
+    options[:from_start] = true if k.include? "+"
+    options[:lines] = k.to_i
   end
 end.parse!
 
@@ -53,8 +54,28 @@ if options[:lines] != nil
       f1 = File.open(filename)
 
       if f1.size <= SEEK_AMOUNT # if the file is smaller than SEEK_AMOUNT, read the whole thing at once
-        f1buffer = f1.readlines.last(options[:lines])
-        puts f1buffer
+        if options[:from_start]
+          f1lines = f1.readlines
+          tail_lines = f1lines.last([f1lines.size - options[:lines], 0].max)
+        else
+          tail_lines = f1.readlines.last(options[:lines])
+        end
+        puts tail_lines
+      elsif options[:from_start]
+
+        seek_amount = SEEK_AMOUNT
+
+        # initialize buffer
+        f1buffer = ""
+
+        # keep looping until we have enough "\n" in the buffer
+        while f1buffer.count("\n") <= options[:lines]
+
+          # read from position, seek_amount amount, a prepend to buffer
+          f1buffer += f1.read(seek_amount)
+        end
+        f1lines = f1buffer.split("\n")
+        tail_lines = f1lines.last([f1lines.size - options[:lines], 0].max).join("\n") + f1.read
       else # otherwise, seek to the end and keep adding text to the buffer until we have enough lines
         seek_amount = SEEK_AMOUNT
 
@@ -84,9 +105,10 @@ if options[:lines] != nil
           else # otherwise, move position further along and prepare for another read
             f1.seek(-seek_amount, :CUR)
           end
+          tail_lines = f1buffer.split("\n").last(options[:lines]).map{|x| "#{x}\n"}
         end
         # when we split the buffer by \n, we want to add \n back in to each line to produce identical output to File.readlines above
-        puts f1buffer.split("\n").last(options[:lines]).map{|x| "#{x}\n"}
+        puts tail_lines
       end
     rescue Exception => e
       puts e.message
